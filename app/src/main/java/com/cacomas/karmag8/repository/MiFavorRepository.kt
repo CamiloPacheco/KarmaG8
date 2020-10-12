d9314905803e07ef96fd28e64b59e56d98ee2a4b
CamiloPacheco <cacomas@uninorte.edu.co>
2020-10-11 20:13:07 -0500

    only chat missing

--- app/src/main/java/com/cacomas/karmag8/repository/MiFavorRepository.kt
+++ app/src/main/java/com/cacomas/karmag8/repository/MiFavorRepository.kt
@@ -46,95 +46,98 @@ class MiFavorRepository {
         database.child("Mifavor").addValueEventListener(postListener)
     }
     fun updateFavor(favorAux:Favor) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                favorList.clear()
-                for (childDataSnapshot in dataSnapshot.children) {
-                    val favor: Favor = childDataSnapshot.getValue(Favor::class.java)!!
-                    if(favor.user==favorAux.user){
-                        database.child("Mifavor").child(childDataSnapshot.key.toString()).child("state").setValue("Asignado")
-                        database.child("Mifavor").child(childDataSnapshot.key.toString()).child("realizadopor").setValue(favorAux.realizadopor)
+        var key:String=""
+        database.child("Mifavor")
+            .orderByChild("user")
+            .equalTo(favorAux.user)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    dataSnapshot.children.forEach {
+                        //"it" is the snapshot
+                        val key: String = it.key.toString()
+                        database.child("Mifavor").child(key).child("state").setValue("Asignado")
+                        database.child("Mifavor").child(key).child("realizadopor").setValue(favorAux.realizadopor)
                     }
                 }
 
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("MyOut", "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        database.child("Mifavor").addValueEventListener(postListener)
+                override fun onCancelled(p0: DatabaseError) {
+                    //do whatever you need
+                }
+            })
 
     }
     fun endFavor(favorAux:Favor,currentUser:String) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                favorList.clear()
-                for (childDataSnapshot in dataSnapshot.children) {
-                    val favor: Favor = childDataSnapshot.getValue(Favor::class.java)!!
-                    if(favor.user==currentUser) {
-                        database.child("Mifavor").child(childDataSnapshot.key.toString())
-                            .child("terminado1").setValue("1")
-                    }
-                    else {
-                        if (favor.realizadopor == currentUser) {
-                            database.child("Mifavor").child(childDataSnapshot.key.toString())
-                                .child("terminado2").setValue("1")
+        var key:String=""
+        database.child("Mifavor")
+            .orderByChild("user")
+            .equalTo(favorAux.user)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    dataSnapshot.children.forEach {
+                        //"it" is the snapshot
+
+                        val favor: Favor = it.getValue(Favor::class.java)!!
+                        if(favor.user==currentUser) {
+                            database.child("Mifavor").child(it.key.toString())
+                                .child("terminado1").setValue("1")
+                        }
+                        else {
+                            if (favor.realizadopor == currentUser) {
+                                database.child("Mifavor").child(it.key.toString())
+                                    .child("terminado2").setValue("1")
+                            }
+                        }
+                        if (favor.terminado1=="1" || favor.terminado2=="1"){
+                            database.child("Favores realizados").push().setValue(favor)
+                            database.child("Mifavor").child(it.key.toString()).removeValue()
+                            favor.realizadopor?.let { KarmaPoint(it,"1") }
                         }
                     }
-                    if (favor.terminado1=="1" && favor.terminado2=="1"){
-                        database.child("Favores realizados").push().setValue(favor)
-                        database.child("Mifavor").child(childDataSnapshot.key.toString()).removeValue()
-                        favor.realizadopor?.let { KarmaPoint(it,"1") }
-                    }
-
                 }
 
-
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("MyOut", "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        database.child("Mifavor").addValueEventListener(postListener)
+                override fun onCancelled(p0: DatabaseError) {
+                    //do whatever you need
+                }
+            })
 
     }
     fun KarmaPoint(user:String, sw:String) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                favorList.clear()
-                for (childDataSnapshot in dataSnapshot.children) {
-                    val karma: Karma= childDataSnapshot.getValue(Karma::class.java)!!
-                    if(user==karma.user){
-                        if (sw=="1") {
-                            var puntos: Int? =karma.puntos?.toInt()
-                            if (puntos != null) {
-                                puntos=puntos+1
+
+        database.child("Karma")
+            .orderByChild("user")
+            .equalTo(user)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                    dataSnapshot.children.forEach {
+                        //"it" is the snapshot
+                        val karma: Karma= it.getValue(Karma::class.java)!!
+                        if(user==karma.user){
+                            if (sw=="1") {
+                                var puntos: Int? =karma.puntos?.toInt()
+                                if (puntos != null) {
+                                    puntos=puntos+1
+                                }
+                                database.child("Karma").child(it.key.toString())
+                                    .child("puntos").setValue(puntos.toString())
                             }
-                            database.child("Karma").child(childDataSnapshot.key.toString())
-                                .child("puntos").setValue(puntos.toString())
-                        }
-                        else {
-                            database.child("Karma").child(childDataSnapshot.key.toString())
-                                .child("puntos").setValue((karma.puntos?.toInt() ?: -2).toString())
+                            else {
+                                database.child("Karma").child(it.key.toString())
+                                    .child("puntos").setValue((karma.puntos?.toInt() ?: -2).toString())
+                            }
                         }
                     }
                 }
 
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w("MyOut", "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        database.child("Karma").addValueEventListener(postListener)
+                override fun onCancelled(p0: DatabaseError) {
+                    //do whatever you need
+                }
+            })
 
     }
 
